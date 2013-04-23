cb69aa337fb643e2d3205e2c6b1618e7e409bfdf
mbaez <mxbg.py@gmail.com>
2013-04-23 13:33:22 -0400

    Se añade soporte para configurar el modo en cada tag

--- securityjs.js
+++ securityjs.js
@@ -18,10 +18,10 @@ var _securityjs = {};
  * @return {Boolean} True si todos existen todos los permisos,
  *          en caso contrario retorna False.
  */
-_securityjs.contains = function(require, permisos){
+_securityjs.contains = function(require, data){
     for(var i=0; i<require.length; i++){
         var permiso = require[i].trim();
-        var status = (permisos.indexOf(permiso) >= 0);
+        var status = (data.indexOf(permiso) >= 0);
         if(!status){
             return false;
         }
@@ -31,41 +31,42 @@ _securityjs.contains = function(require, permisos){
 
 /**
  * Este método se encarga de verificar los permisos del usuario y
- * eliminar los componentes html sobre los cuales los no posee permisos.
+ * realizar una opreacion dependiendo del modo asociado al tag HTML.
  *
- * @author <a href="mailto:mxbg.py@gmail.com">Maximiliano Báez</a>
- * @params options {Object}
- * @config permisos {String[]} La lista de permisos con los que se cuenta.
- * @config [handler]{Function} Función que se invoca cuando el usuario no
- *          posee permisos.
- * @config [mode]{String} Los modos en los que se puede procesar el
- *         dom asociado al permiso. Default 'remove'. <br/>
+ *<br/>
+ * Los modos que se pueden procesar son :<br/>
  * <ul>
  * <li>remove : Se elimina el dom asociado al permiso</li>
  * <li>disable : Para añadir el atributo disable al dom asociado al permiso</li>
  * <li>hide : Para ocultar el dom asociado al permiso</li>
  * <li>error : Se carga un mensaje de error en el dom asociado al permiso</li>
  * </ul>
+ *
+ * @author <a href="mailto:mxbg.py@gmail.com">Maximiliano Báez</a>
+ * @params options {Object}
+ * @config data {String[]} La lista de permisos con los que se cuenta.
+ * @config [handler]{Function} Función que se invoca cuando el usuario no
+ *          posee permisos.
  */
 _securityjs.initialize = function (options){
-    var mode;
-    if(!options || options && !options['mode']){
-        mode = 'remove';
-    }else {
-        mode =  options.mode;
-    }
-    //se obtiene todos los permisos
-    var permisos = options.permisos;
+    //se obtienen todos los permisos
+    var data = options.data;
     var thiz = this;
     //se obtienen todos los elementos que tienen el atributo require
     $('[data-require]').each(function(){
         // se obtienen los permisos definidos en atributo require del
         // componente de html.
         var toSplit = $(this).attr('data-require');
+        //se obtiene el modo de procesar el tag
+        var mode = $(this).attr('data-require-mode');
+        //por default se toma remove
+        if(typeof mode == "undefined"){
+            mode="remove";
+        }
         //se separa los elementos
         var require = toSplit.split(';');
         //se verifica si existen posee los permisos.
-        if(!thiz.contains(require, permisos)){
+        if(!thiz.contains(require, data)){
             if(mode == 'remove'){
                 //se elimina el elemento si el usuario no posee los
                 //permisos necesarios para ver el componente.
@@ -75,7 +76,9 @@ _securityjs.initialize = function (options){
             }else if(mode == 'hide'){
                 $(this).attr('style','visibility:hidden');
             }else if(mode == 'error'){
-                $(this).html("No posee los permisos para realizar esta acción");
+                $(this).html("Forbbiden!!");
+            }else{
+                throw  'Value of require-mode `'+mode+'` is not valid';
             }
             //se invoca al hanler
             if(options && options.handler instanceof Function){
